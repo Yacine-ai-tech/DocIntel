692b43624eb1928af73140136e6e36acf404cbb2
Seybou Siddo Yacine <146183381+Yacine-ai-tech@users.noreply.github.com>
2026-07-22 11:05:00 +0100

    docs: clean telemetry guidelines

--- frontend/e2e/test_error.spec.ts
+++ frontend/e2e/test_error.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@playwright/test';
+
+const BASE_URL = process.env.TEST_BASE_URL || BASE_URL + '';
 test('get error', async ({ page }) => {
   page.on('console', msg => console.log('PAGE LOG:', msg.text()));
   page.on('pageerror', error => console.log('PAGE ERROR:', error.message));
-  await page.goto('http://localhost:5174', { waitUntil: 'networkidle' });
+  await page.goto('/', { waitUntil: 'domcontentloaded' });
 });
